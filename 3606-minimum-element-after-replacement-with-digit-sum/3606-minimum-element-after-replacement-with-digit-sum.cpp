98558406faf80365acb3de38a9094a79bdf8da85
Algo-Master <157021882+Algo-Master@users.noreply.github.com>
2024-09-29 18:54:00 +0530

    Time: 3 ms (100%), Space: 27.2 MB (91.67%) - LeetHub

---
+++ 3606-minimum-element-after-replacement-with-digit-sum/3606-minimum-element-after-replacement-with-digit-sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minElement(vector<int>& nums) {
+        
+        int n = nums.size(), mn = INT_MAX;
+
+        for(int i = 0; i<n; i++) {
+
+            int sum = 0;
+
+            while(nums[i]>0) {
+                sum += nums[i]%10;
+                nums[i] /= 10;
+            }
+            mn = min(mn, sum);
+        }
+
+        return mn;
+    }
+};
