127108306ac180eab79cf52355204a20d806cb4c
Algo-Master <157021882+Algo-Master@users.noreply.github.com>
2024-12-24 13:35:36 +0530

    Time: 10 ms (19.25%), Space: 19.1 MB (17.62%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool fg = true;
+    vector<int> adj[2000];
+
+    void toposort_dfs(int node, stack<int>&st, vector<int> &col) {
+        col[node] = 1;
+
+        for(auto &child: adj[node]) {
+            if(col[child] == 0) {
+                toposort_dfs(child, st, col);
+            }
+            else if(col[child] == 1) fg = 0;
+        }
+        col[node] = 2;
+        st.push(node);
+    }
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int>col(2000, 0);
+        int n = prerequisites.size();
+        for(int i = 0; i < n; i++) {
+            vector<int> order = prerequisites[i];
+            adj[order[1]].push_back(order[0]);
+        }
+        stack<int> st;
+        for(int i = 0; i < numCourses; i++) {
+            if(col[i] == 0) {
+                toposort_dfs(i, st, col);
+            }
+        }
+
+        vector<int> toposorted;
+
+        if(!fg) {
+            return toposorted;
+        }
+        
+        while(!st.empty()) {
+            toposorted.push_back(st.top());
+            st.pop();
+        }
+
+        return toposorted;
+    }
+};
