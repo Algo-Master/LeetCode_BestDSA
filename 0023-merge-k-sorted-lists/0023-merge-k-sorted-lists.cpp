c1042ada0ec461f2550a2e146fb2291850a932d0
Algo-Master <157021882+Algo-Master@users.noreply.github.com>
2024-12-27 01:24:06 +0530

    Time: 95 ms (16.98%), Space: 18 MB (95.31%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -5,57 +5,59 @@
  *     ListNode *next;
  *     ListNode() : val(0), next(nullptr) {}
  *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x, next) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
 class Solution {
 public:
     ListNode* merge_lists(ListNode* l1, ListNode* l2) {
+
         if (!l1) return l2;
         if (!l2) return l1;
 
         ListNode* head = nullptr;
-        ListNode* tail = nullptr;
+        ListNode* curr = nullptr;
 
         if (l1->val < l2->val) {
-            head = tail = l1;
+            head = curr = l1;
             l1 = l1->next;
         } else {
-            head = tail = l2;
+            head = curr = l2;
             l2 = l2->next;
         }
 
         while (l1 && l2) {
             if (l1->val < l2->val) {
-                tail->next = l1;
-                tail = l1;
+                curr->next = l1;
+                curr = l1;
                 l1 = l1->next;
             } else {
-                tail->next = l2;
-                tail = l2;
+                curr->next = l2;
+                curr = l2;
                 l2 = l2->next;
             }
         }
 
         if (l1) {
-            tail->next = l1;
+            curr->next = l1;
         } else if (l2) {
-            tail->next = l2;
+            curr->next = l2;
         }
 
         return head;
     }
-
     ListNode* mergeKLists(vector<ListNode*>& lists) {
+        
         int n = lists.size();
 
-        if (n == 0) return nullptr;
-        if (n == 1) return lists[0];
+        if(n == 0) return nullptr;
+        else if(n == 1) return lists[0];
 
-        ListNode* mergedList = lists[0];
-        for (int i = 1; i < n; ++i) {
-            mergedList = merge_lists(mergedList, lists[i]);
+        ListNode* curr_list = merge_lists(lists[0], lists[1]);
+        for(int i = 2; i < n; i++) {
+            curr_list = merge_lists(curr_list, lists[i]);
         }
-        return mergedList;
+
+        return curr_list;
     }
 };
