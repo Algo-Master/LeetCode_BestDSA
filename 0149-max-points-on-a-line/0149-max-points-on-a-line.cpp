db45fb2d74c7650fb2466da029c8cacc1926ca0d
Algo-Master <157021882+Algo-Master@users.noreply.github.com>
2024-12-30 20:46:57 +0530

    Time: 39 ms (51.13%), Space: 24.2 MB (22.12%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int maxPoints(vector<vector<int>>& points) {
-        int n=points.size();
-        if(n<=2) return n;
+        int n = points.size();
+        if(n <= 2) return n;
         int ans = 0;
 
-        for(auto it1 : points){
+        for(auto &a: points){
             unordered_map<double,int> mp;
-            double x1 = it1[0], y1 = it1[1];
-            for(auto it2 : points){   
-                if(it2 == it1) continue;
-                double x2 = it2[0], y2 = it2[1];
+            double x1 = a[0], y1 = a[1];
+            for(auto &b: points){   
+                if(b == a) continue;
+                double x2 = b[0], y2 = b[1];
                 double slope;
                 if(x2-x1 == 0){
                     slope = INT_MAX; // slope is infinity for vertical line
