76e4b2d99104eb4248f0447e288f8f16da0a4e50
Algo-Master <157021882+Algo-Master@users.noreply.github.com>
2024-12-30 20:49:22 +0530

    Time: 43 ms (41.79%), Space: 24.2 MB (22.12%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -6,10 +6,10 @@ public:
         int ans = 0;
 
         for(auto &a: points){
-            unordered_map<double,int> mp;
+            unordered_map<double,int> m;
             double x1 = a[0], y1 = a[1];
             for(auto &b: points){   
-                if(b == a) continue;
+                if(b == a) continue;  // Points with identical x and y are not considered to be distinct
                 double x2 = b[0], y2 = b[1];
                 double slope;
                 if(x2-x1 == 0){
@@ -17,8 +17,8 @@ public:
                 }else{
                     slope = (y2-y1)/(x2-x1);   
                 }
-                mp[slope]++;
-                ans = max(ans,mp[slope]);
+                m[slope]++;
+                ans = max(ans,m[slope]);
             }
         }
         return ans+1; //including point i
