55e61247ffe4cf42405e6fe29a12b783b5f81bea
Algo-Master <157021882+Algo-Master@users.noreply.github.com>
2025-01-05 13:44:46 +0530

    Time: 0 ms (100%), Space: 10.8 MB (65.77%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -21,7 +21,28 @@ private:
 public:
     vector<int> preorderTraversal(TreeNode* root) {
 
-        if(root != nullptr) preorder(root);
-        return traversal;
+        // if(root != nullptr) preorder(root);
+        // return traversal;
+
+        // Iterative way
+
+        stack<TreeNode*> st;
+        st.push(root);
+
+        vector<int> ans;
+        if(root == nullptr) return ans;
+
+        while(!st.empty()) {
+
+            TreeNode* node = st.top();
+            st.pop();
+
+            ans.push_back(node->val);
+
+            if(node->right != nullptr) st.push(node->right);
+            if(node->left != nullptr) st.push(node->left);
+        }
+
+        return ans;
     }
 };
